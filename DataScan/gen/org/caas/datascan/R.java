81801078a10fd7b2fb2bf43dd2c0ddeff712ca43
William <admswilliamdong@gmail.com>
2015-10-28 11:01:22 +0800

    Changed the color of Action Bar

--- DataScan/gen/org/caas/datascan/R.java
+++ DataScan/gen/org/caas/datascan/R.java
@@ -11,6 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
+        public static final int actionbar_background=0x7f050024;
+        public static final int actionbar_text=0x7f050025;
         public static final int bg_color=0x7f050000;
         public static final int blue_black=0x7f05001e;
         public static final int contents_text=0x7f050001;
@@ -51,7 +53,7 @@ public final class R {
         public static final int white=0x7f05001b;
     }
     public static final class dimen {
-        public static final int WelcomeFontSize=0x7f060005;
+        public static final int WelcomeFontSize=0x7f060006;
         /**  Default screen margins, per the Android Design guidelines. 
 
          Example customization of dimensions originally defined in res/values/dimens.xml
@@ -62,59 +64,62 @@ public final class R {
         public static final int activity_horizontal_margin=0x7f060000;
         public static final int activity_vertical_margin=0x7f060001;
         public static final int button_height=0x7f060002;
+        public static final int header_text_font_size=0x7f060005;
         public static final int list_item_content_text=0x7f060004;
         public static final int login_btn_margin_top=0x7f060003;
     }
     public static final class drawable {
         public static final int bg_login_btn_default=0x7f020000;
         public static final int bg_login_btn_pressed=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int login_button=0x7f020003;
-        public static final int navbar=0x7f020004;
+        public static final int ic_back=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int login_button=0x7f020004;
+        public static final int navbar=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f070028;
+        public static final int action_settings=0x7f070029;
         /**  Messages IDs 
          */
         public static final int auto_focus=0x7f070000;
-        public static final int btnBack1=0x7f070020;
-        public static final int btnChooseTemplate=0x7f070014;
+        public static final int btnBack1=0x7f070021;
+        public static final int btnChooseTemplate=0x7f070015;
         public static final int btnChooseTemplate1=0x7f07000e;
-        public static final int btnExit=0x7f070010;
-        public static final int btnFindInTemplate=0x7f070013;
-        public static final int btnRefreshTemplateList=0x7f070027;
-        public static final int btnSave1=0x7f070021;
-        public static final int btnSaveTemplateColumn=0x7f070026;
-        public static final int btnScan=0x7f070015;
+        public static final int btnExit=0x7f070011;
+        public static final int btnFindInTemplate=0x7f070014;
+        public static final int btnRefreshTemplateList=0x7f070028;
+        public static final int btnSave1=0x7f070022;
+        public static final int btnSaveTemplateColumn=0x7f070027;
+        public static final int btnScan=0x7f070016;
         public static final int btnSearch=0x7f07000f;
-        public static final int buttonChooseTemplate=0x7f070018;
-        public static final int chkColumnChoose=0x7f070025;
+        public static final int buttonChooseTemplate=0x7f070019;
+        public static final int chkColumnChoose=0x7f070026;
         public static final int decode=0x7f070001;
         public static final int decode_failed=0x7f070002;
         public static final int decode_succeeded=0x7f070003;
-        public static final int edtComment=0x7f07001d;
-        public static final int edtData=0x7f07001b;
-        public static final int edtScanColumn=0x7f070012;
+        public static final int edtComment=0x7f07001e;
+        public static final int edtData=0x7f07001c;
+        public static final int edtScanColumn=0x7f070013;
         public static final int encode_failed=0x7f070004;
         public static final int encode_succeeded=0x7f070005;
         public static final int launch_product_query=0x7f070006;
-        public static final int listViewColumnData=0x7f070019;
-        public static final int listViewTemplate=0x7f070022;
-        public static final int listViewTemplateColumn=0x7f070023;
+        public static final int listViewColumnData=0x7f07001a;
+        public static final int listViewTemplate=0x7f070023;
+        public static final int listViewTemplateColumn=0x7f070024;
         public static final int preview_view=0x7f07000c;
         public static final int quit=0x7f070007;
-        public static final int rdoColumnScan=0x7f070024;
+        public static final int rdoColumnScan=0x7f070025;
         public static final int restart_preview=0x7f070008;
         public static final int return_scan_result=0x7f070009;
         public static final int search_book_contents_failed=0x7f07000a;
         public static final int search_book_contents_succeeded=0x7f07000b;
-        public static final int textViewLoading=0x7f070017;
-        public static final int textViewWelcome=0x7f070016;
-        public static final int txtColumnName=0x7f07001e;
-        public static final int txtColumnValue=0x7f07001f;
-        public static final int txtComment=0x7f07001c;
-        public static final int txtData=0x7f07001a;
-        public static final int txtTemplateName=0x7f070011;
+        public static final int textView1=0x7f070010;
+        public static final int textViewLoading=0x7f070018;
+        public static final int textViewWelcome=0x7f070017;
+        public static final int txtColumnName=0x7f07001f;
+        public static final int txtColumnValue=0x7f070020;
+        public static final int txtComment=0x7f07001d;
+        public static final int txtData=0x7f07001b;
+        public static final int txtTemplateName=0x7f070012;
         public static final int viewfinder_view=0x7f07000d;
     }
     public static final class layout {
